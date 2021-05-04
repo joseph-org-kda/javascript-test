e960892ed2dd103b8e2e9e53965851e787dfaa75
JosephKabemba <joseph.kabemba@academy.kinshasadigital.com>
2021-05-04 15:24:49 +0200

    add test

--- sum.js
+++ sum.js
@@ -1,14 +1,10 @@
 const sum = (...numbers) => {
-  if (numbers.length === 0) {
-    throw new Error("Array is empty");
-  }
-
   const result = numbers.reduce((previous, current) => {
     if (isNaN(current)) {
       throw new TypeError("Not a number");
     }
     return previous + current;
-  }, 0);
+  });
 
   return result;
 };
